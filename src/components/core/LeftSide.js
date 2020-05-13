8973a36339f431ad9d524a7816ed555034032c4c
talentdevsilvio <polaris.center@outlook.com>
2020-05-13 10:37:18 +0200

    Update Logo.png

--- src/components/core/LeftSide.js
+++ src/components/core/LeftSide.js
@@ -64,7 +64,7 @@ const LeftSide = ({ history, data }) => {
   return (
     <LetfWrapper>
       <div style={{ position: 'fixed' }}>
-        <img src="logo" alt="Logo" />
+        <img src="logo.png" alt="Logo" />
         <div
           role="search"
           style={{
