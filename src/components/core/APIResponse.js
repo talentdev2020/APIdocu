7901b57376b6031c335b4a365d2ace36d81ffb66
talentdevsilvio <polaris.center@outlook.com>
2020-05-13 16:43:53 +0200

    Add ExpandAll

--- src/components/core/APIResponse.js
+++ src/components/core/APIResponse.js
@@ -83,32 +83,133 @@ const Li = styled.li`
   padding: unset;
 `;
 const Collapse = styled.div`
-  top: 1px;
-  left: -1.5em;
   padding-right: 6px;
   padding-left: 6px;
+  cursor: pointer;
+
   &:after {
     content: '+';
-    ${(props) => props.isExpand && 'content: ' - ';'}
+    ${(props) =>
+      props.isExpand &&
+      css`
+        content: '-';
+      `}
     cursor: pointer;
   }
 `;
-function handleClick(index) {}
-const APIResponse = ({ response, isVisible }) => {
-  const [body, setBody] = useState('');
-  const [bodytype, setBodyType] = useState('200');
-  const [requestBody, setRequestBody] = useState('');
-  const [copy, setCopied] = useState('Copy');
-  const [requestcopy, setRequestCopied] = useState('Copy');
 
+const APIResponse = ({ request, response, isVisible }) => {
+  const [body, setBody] = useState('');
+  const [responseBody, setResponseBody] = useState('');
+  const [bodytype, setBodyType] = useState('200');
+
+  const handleClick = (index) => {
+    const temp = body.map((item, i) => {
+      item.isMarked = false;
+      if (index === i) item.isExpand = !item.isExpand;
+      return item;
+    });
+    setBody(temp);
+    const data = makeResponse(0, 0);
+    console.log(data);
+    setResponseBody(data);
+  };
+
+  const expandAll = () => {
+    const newData = body.map((item) => {
+      item.isExpand = true;
+      item.isMarked = false;
+      return item;
+    });
+    setBody(newData);
+    const data = makeResponse(0, 0);
+    console.log(data);
+    setResponseBody(data);
+  };
+  const collapseAll = () => {
+    const newData = body.map((item) => {
+      item.isExpand = false;
+      item.isMarked = false;
+      return item;
+    });
+    setBody(newData);
+    const data = makeResponse(0, 0);
+    console.log(data);
+    setResponseBody(data);
+  };
+
+  const makeResponse = (start, depth) => {
+    let childflag = 0;
+    return (
+      body &&
+      body.map((item, index) => {
+        if (index < start) return <></>;
+        if (childflag === 1) return <></>;
+
+        if (item.isMarked) return <></>;
+        let left = 16 * depth;
+        item.isMarked = true;
+        if (item.name.includes('{') || item.name.includes('[')) {
+          return (
+            <Li key={index + start + 'li' + item.name}>
+              <div
+                key={index + 'div' + item.name}
+                style={{
+                  display: 'flex',
+                  position: 'relative',
+                  paddingLeft: left + 'px',
+                }}
+              >
+                {index !== 0 && (
+                  <Collapse
+                    isExpand={item.isExpand}
+                    onClick={(e) => handleClick(index)}
+                  />
+                )}
+                <ResponsiveSpan>{item.name}</ResponsiveSpan>
+              </div>
+
+              <ul
+                className={item.isExpand ? '' : 'hide'}
+                key={index + 'ul' + item.name}
+              >
+                {makeResponse(index + 1, depth + 1)}
+              </ul>
+            </Li>
+          );
+        }
+        if (item.name.includes('}') || item.name.includes(']')) {
+          childflag = 1;
+          left = 16 * (depth - 1);
+          return (
+            <Li
+              key={index + start + 'res' + item.name}
+              style={{ paddingLeft: left + 'px' }}
+            >
+              <ResponsiveSpan>{item.name}</ResponsiveSpan>
+            </Li>
+          );
+          //  parentflag = 0;
+        }
+        //  if (parentflag === 1) return <></>;
+        return (
+          <Li
+            key={index + start + 'res' + item.name}
+            style={{ paddingLeft: left + 'px' }}
+          >
+            <ResponsiveSpan>{item.name}</ResponsiveSpan>
+          </Li>
+        );
+      })
+    );
+  };
   useEffect(() => {
     let string = '';
     if (bodytype === '200') {
       string =
         response &&
         response
-          .split('},')
-          .join('}')
+
           .split('],')
           .join(']')
           .split(',')
@@ -116,9 +217,21 @@ const APIResponse = ({ response, isVisible }) => {
           .split('{')
           .join('{, ')
           .split('}')
-          .join('}, ')
+          .join(', }')
+          .split('[')
+          .join('[, ')
+          .split(']')
+          .join('], ')
           .split(', ');
     }
+
+    string =
+      string &&
+      string.filter((item) => {
+        return item !== '';
+      });
+    string && string.push('}');
+    console.log(string);
     let depth = 0;
     let newData =
       string &&
@@ -134,87 +247,28 @@ const APIResponse = ({ response, isVisible }) => {
       });
     setBody(newData);
   }, [response, bodytype]);
-  const Test = (start, depth) => {
-    let childflag = 0;
-    let parentflag = 0;
-    return body.map((item, index) => {
-      if (childflag === 1) return <></>;
-      if (index <= start) return <></>;
-      // if (item.isMarked) return <></>;
-      const left = 16 * depth;
-
-      console.log(item.name);
-      if (item.name.includes('{') || item.name.includes('[')) {
-        parentflag = 1;
-        body[index].isMarked = true;
-        return (
-          <Li key={index + 'li' + item.name}>
-            <div
-              style={{
-                display: 'flex',
-                position: 'relative',
-                paddingLeft: left + 'px',
-              }}
-            >
-              <Collapse
-                isExpand={item.isExpand}
-                onClick={(e) => handleClick(index)}
-              />
-              <ResponsiveSpan>{item.name}</ResponsiveSpan>
-            </div>
-            <ul>{depth < 4 ? Test(index, depth + 1) : ''}</ul>
-          </Li>
-        );
-      }
-      if (item.name.includes('}') || item.name.includes(']')) {
-        childflag = 1;
-        parentflag = 0;
-      }
-      if (parentflag === 1) return <></>;
-      body[index].isMarked = true;
-      return (
-        <Li
-          key={index + 'res' + item.name}
-          style={{ paddingLeft: left + 'px' }}
-        >
-          <ResponsiveSpan>{item.name}</ResponsiveSpan>
-        </Li>
-      );
-    });
-  };
-  useEffect(() => {
-    const string =
-      response &&
-      response
-        .split('{')
-        .join('{<div class="response">')
-        .split('}')
-        .join('</div>}')
-        .split('<div class="response">')
-        .join('<div class="response">')
-        .split('</div>')
-        .join('</div>')
-        .split(',')
-        .join(',<br/>');
-    setRequestBody(string);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  // useEffect(() => {
+  //   const string =
+  //     response &&
+  //     response
+  //       .split('{')
+  //       .join('{<div class="response">')
+  //       .split('}')
+  //       .join('</div>}')
+  //       .split('<div class="response">')
+  //       .join('<div class="response">')
+  //       .split('</div>')
+  //       .join('</div>')
+  //       .split(',')
+  //       .join(',<br/>');
+  //   setRequestBody(string);
+  //   // eslint-disable-next-line react-hooks/exhaustive-deps
+  // }, []);
 
   const changeContentResponse = (type) => {
     setBodyType(type);
   };
-  const setCopy = () => {
-    setCopied('Copied');
-    setTimeout(relaseCopied, 3000);
-  };
-  const setRequestCopy = () => {
-    setRequestCopied('Copied');
-    setTimeout(relaseCopied, 3000);
-  };
-  const relaseCopied = () => {
-    setCopied('Copy');
-    setRequestBody('Copy');
-  };
+
   return (
     <APIResponseWrapper>
       {!isVisible && (
@@ -227,19 +281,17 @@ const APIResponse = ({ response, isVisible }) => {
         </div>
       )}
 
-      {requestBody && (
+      {request && (
         <APIResponseBody>
           <JsonWrapper>
             <JsonWrapperHeader>Content Type</JsonWrapperHeader>application/json
           </JsonWrapper>
           <CopyBoard>
-            <CopyToClipboard text={requestBody} onCopy={setRequestCopy}>
-              <CopyButton>{requestcopy}</CopyButton>
+            <CopyToClipboard text={request}>
+              <CopyButton key="copy">{request}</CopyButton>
             </CopyToClipboard>
-            <CopyButton>Expand All</CopyButton>
-            <CopyButton>Collapse All</CopyButton>
           </CopyBoard>
-          <P dangerouslySetInnerHTML={{ __html: requestBody }}></P>
+          <P dangerouslySetInnerHTML={{ __html: request }}></P>
         </APIResponseBody>
       )}
 
@@ -265,13 +317,14 @@ const APIResponse = ({ response, isVisible }) => {
         <APIResponseBody>
           <JsonWrapper>application/json</JsonWrapper>
           <CopyBoard>
-            <CopyToClipboard text={body} onCopy={setCopy}>
-              <CopyButton>{copy}</CopyButton>
+            <CopyToClipboard text={body}>
+              <CopyButton>Copy</CopyButton>
             </CopyToClipboard>
-            <CopyButton>Expand All</CopyButton>
-            <CopyButton>Collapse All</CopyButton>
+            <CopyButton onClick={expandAll}>Expand All</CopyButton>
+            <CopyButton onClick={collapseAll}>Collapse All</CopyButton>
           </CopyBoard>
-          {Test(0, 1)}
+          <div>{responseBody ? responseBody : makeResponse(0, 0)}</div>
+
           {/* <P dangerouslySetInnerHTML={{ __html: body }}></P> */}
         </APIResponseBody>
       )}
