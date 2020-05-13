f7b8e798dadbe5db7c5e18cbc284e99c962043cc
talentdevsilvio <polaris.center@outlook.com>
2020-05-13 17:22:31 +0200

    Update API

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,5 +1,5 @@
 import { apiPath } from './config';
 import axios from 'axios';
 export const getData = () => {
-  // return axios.get('https://cors-anywhere.herokuapp.com/' + apiPath);
+  return axios.get('https://cors-anywhere.herokuapp.com/' + apiPath);
 };
