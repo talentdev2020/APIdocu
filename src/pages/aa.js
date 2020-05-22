5cc7c01586ef200ab1f5f03e9aa7eb31f8c11986
talentdevsilvio <polaris.center@outlook.com>
2020-05-22 04:57:16 +0200

    Add htaccess

---
+++ src/pages/aa.js
@@ -0,0 +1,136 @@
+const a = {
+  SearchResult: {
+    Items: [
+      {
+        ASIN: 'B0798B31TC',
+        DetailPageURL:
+          'https://www.amazon.com/dp/B0798B31TC?tag=save039-20&linkCode=osi&th=1&psc=1',
+        ItemInfo: {
+          Title: {
+            DisplayValue:
+              'Dyson Cyclone V10 Motorhead Lightweight Cordless Stick Vacuum Cleaner',
+            Label: 'Title',
+            Locale: 'en_US',
+          },
+        },
+      },
+      {
+        ASIN: 'B07R761N63',
+        DetailPageURL:
+          'https://www.amazon.com/dp/B07R761N63?tag=save039-20&linkCode=osi&th=1&psc=1',
+        ItemInfo: {
+          Title: {
+            DisplayValue:
+              'Dyson V7 Animal Pro+ Cordless Vacuum Cleaner-Extra Tools for Homes with Pets, Rechargeable, Lightweight, Powerful Suction, V7 Animal Pro +, Blue',
+            Label: 'Title',
+            Locale: 'en_US',
+          },
+        },
+      },
+      {
+        ASIN: 'B081P49HJZ',
+        DetailPageURL:
+          'https://www.amazon.com/dp/B081P49HJZ?tag=save039-20&linkCode=osi&th=1&psc=1',
+        ItemInfo: {
+          Title: {
+            DisplayValue:
+              'Dyson Airwrap Complete Styler for Multiple Hair Types and Styles, Fuchsia',
+            Label: 'Title',
+            Locale: 'en_US',
+          },
+        },
+      },
+      {
+        ASIN: 'B07NX8XBMP',
+        DetailPageURL:
+          'https://www.amazon.com/dp/B07NX8XBMP?tag=save039-20&linkCode=osi&th=1&psc=1',
+        ItemInfo: {
+          Title: {
+            DisplayValue:
+              'Dyson V11 Torque Drive Cordless Vacuum Cleaner, Blue',
+            Label: 'Title',
+            Locale: 'en_US',
+          },
+        },
+      },
+      {
+        ASIN: 'B01FIG1JIM',
+        DetailPageURL:
+          'https://www.amazon.com/dp/B01FIG1JIM?tag=save039-20&linkCode=osi&th=1&psc=1',
+        ItemInfo: {
+          Title: {
+            DisplayValue: 'Dyson Supersonic Hair Dryer, Iron/Fuchsia, 1200W',
+            Label: 'Title',
+            Locale: 'en_US',
+          },
+        },
+      },
+      {
+        ASIN: 'B07BJDSKS8',
+        DetailPageURL:
+          'https://www.amazon.com/dp/B07BJDSKS8?tag=save039-20&linkCode=osi&th=1&psc=1',
+        ItemInfo: {
+          Title: {
+            DisplayValue:
+              'Dyson Pure Cool, DP04-HEPA Air Purifier and Fan WiFi-Enabled, Large Rooms, Automatically Removes Allergens, Pollutants, Dust, Mold, VOCs, White/Silver',
+            Label: 'Title',
+            Locale: 'en_US',
+          },
+        },
+      },
+      {
+        ASIN: 'B06XG7WKKL',
+        DetailPageURL:
+          'https://www.amazon.com/dp/B06XG7WKKL?tag=save039-20&linkCode=osi&th=1&psc=1',
+        ItemInfo: {
+          Title: {
+            DisplayValue: 'Dyson V8 Animal Cordless Stick Vacuum Cleaner, Iron',
+            Label: 'Title',
+            Locale: 'en_US',
+          },
+        },
+      },
+      {
+        ASIN: 'B0748TR6Q1',
+        DetailPageURL:
+          'https://www.amazon.com/dp/B0748TR6Q1?tag=save039-20&linkCode=osi&th=1&psc=1',
+        ItemInfo: {
+          Title: {
+            DisplayValue: 'Dyson V7 Trigger Cord-Free Handheld Vacuum Cleaner',
+            Label: 'Title',
+            Locale: 'en_US',
+          },
+        },
+      },
+      {
+        ASIN: 'B01N4BI77I',
+        DetailPageURL:
+          'https://www.amazon.com/dp/B01N4BI77I?tag=save039-20&linkCode=osi&th=1&psc=1',
+        ItemInfo: {
+          Title: {
+            DisplayValue:
+              'Dyson Upright Vacuum Cleaner, Ball Animal 2, Iron/Purple',
+            Label: 'Title',
+            Locale: 'en_US',
+          },
+        },
+      },
+      {
+        ASIN: 'B01MSZ036Q',
+        DetailPageURL:
+          'https://www.amazon.com/dp/B01MSZ036Q?tag=save039-20&linkCode=osi&th=1&psc=1',
+        ItemInfo: {
+          Title: {
+            DisplayValue:
+              'Dyson V7 Motorhead Cordless Stick Vacuum Cleaner, Fuchsia (227591-01)',
+            Label: 'Title',
+            Locale: 'en_US',
+          },
+        },
+      },
+    ],
+    SearchURL:
+      'https://www.amazon.com/s?k=dyson&rh=p_n_availability%3A-1&tag=save039-20&linkCode=osi',
+    TotalResultCount: 146,
+  },
+};
